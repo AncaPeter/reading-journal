6c84d7386d320c9447ba5155497fde8ef7879395
AncaPeter <ancapeter90@gmail.com>
2022-10-04 18:44:21 +0300

    * initial commit

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent implements OnInit {
+  book: Book = {
+    id: 1,
+    name: 'Regatul spinilor si al trandafirilor'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
